912e780fabd254e548edc79e3340325eec1a3b2f
Jasper Chan <jasperchan515@gmail.com>
2023-10-06 01:55:36 -0700

    feature: block reads and writes
    passes full test suite

--- main.c
+++ main.c
@@ -23,6 +23,7 @@
 //
 // Example of an SPI bus slave using the PL022 SPI interface
 
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include "pico/stdlib.h"
@@ -37,6 +38,106 @@
 #define PICO_DEFAULT_SPI_RX_PIN 20
 #define PICO_DEFAULT_SPI_CSN_PIN 1
 
+
+#define SECTOR_SIZE 512
+
+typedef struct Sector {
+    uint32_t block;
+    uint8_t data[SECTOR_SIZE];
+    uint16_t crc;
+    struct Sector* next;
+} Sector;
+
+// FAT16 sector pointer or something, do we need this?
+Sector sec1 = {
+    0x01,
+    {0xF8, 0xFF, 0xFF, 0xFF},
+    0,
+    NULL
+};
+
+// FAT16 header
+Sector header = {
+    0x00,
+    {
+        0xEB, 0x3C, 0x90, 0x6D, 0x6B, 0x66, 0x73, 0x2E, 0x66, 0x61, 0x74, 0x00, 0x02, 0x01, 0x01, 0x00,
+        0x01, 0x00, 0x02, 0x00, 0x18, 0xF8, 0x18, 0x00, 0x20, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x80, 0x00, 0x29, 0xED, 0xBD, 0xCC, 0xF0, 0x4E, 0x4F, 0x20, 0x4E, 0x41,
+        0x4D, 0x45, 0x20, 0x20, 0x20, 0x20, 0x46, 0x41, 0x54, 0x31, 0x36, 0x20, 0x20, 0x20, 0x0E, 0x1F,
+        0xBE, 0x5B, 0x7C, 0xAC, 0x22, 0xC0, 0x74, 0x0B, 0x56, 0xB4, 0x0E, 0xBB, 0x07, 0x00, 0xCD, 0x10,
+        0x5E, 0xEB, 0xF0, 0x32, 0xE4, 0xCD, 0x16, 0xCD, 0x19, 0xEB, 0xFE, 0x54, 0x68, 0x69, 0x73, 0x20,
+        0x69, 0x73, 0x20, 0x6E, 0x6F, 0x74, 0x20, 0x61, 0x20, 0x62, 0x6F, 0x6F, 0x74, 0x61, 0x62, 0x6C,
+        0x65, 0x20, 0x64, 0x69, 0x73, 0x6B, 0x2E, 0x20, 0x20, 0x50, 0x6C, 0x65, 0x61, 0x73, 0x65, 0x20,
+        0x69, 0x6E, 0x73, 0x65, 0x72, 0x74, 0x20, 0x61, 0x20, 0x62, 0x6F, 0x6F, 0x74, 0x61, 0x62, 0x6C,
+        0x65, 0x20, 0x66, 0x6C, 0x6F, 0x70, 0x70, 0x79, 0x20, 0x61, 0x6E, 0x64, 0x0D, 0x0A, 0x70, 0x72,
+        0x65, 0x73, 0x73, 0x20, 0x61, 0x6E, 0x79, 0x20, 0x6B, 0x65, 0x79, 0x20, 0x74, 0x6F, 0x20, 0x74,
+        0x72, 0x79, 0x20, 0x61, 0x67, 0x61, 0x69, 0x6E, 0x20, 0x2E, 0x2E, 0x2E, 0x20, 0x0D, 0x0A, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x55, 0xAA
+    },
+    0,
+    &sec1,
+};
+
+void setCrc(Sector* sector) {
+    // TODO: Set CRC
+    // Currently the arduino SD lib ignores this
+    return;
+}
+
+Sector* getBlock(uint32_t block) {
+    Sector* ptr = &header;
+    while(1) {
+        if(ptr->block == block) {
+            setCrc(ptr);
+            return ptr;
+        }
+        if(ptr->next == NULL)
+            return NULL;
+        ptr = ptr->next;
+    }
+}
+
+Sector* allocateBlock(uint32_t block) {
+    Sector* ptr = &header;
+    while(1) {
+        if(ptr->block == block) {
+            setCrc(ptr);
+            return ptr;
+        }
+        if(ptr->next == NULL)
+            break;
+        ptr = ptr->next;
+    }
+    printf("Allocating block %d\n", block);
+    ptr->next = (Sector*)malloc(sizeof(Sector));
+    ptr = ptr->next;
+    if (ptr == NULL) {
+        printf("Allocation failed\n");
+        return NULL;
+    }
+    ptr->block = block;
+    ptr->next = NULL;
+    return ptr;
+}
 
 void printbuf(uint8_t buf[], size_t len) {
     int i;
@@ -53,9 +154,11 @@ void printbuf(uint8_t buf[], size_t len) {
     }
 }
 
+// In most cases we want to send 0xFF if we are idling
+uint8_t spi_tx_idle_value = 0xFF;
 void spi_irq_handler() {
     while(spi_get_hw(spi_default)->ris & 0b1000) {
-        spi_get_hw(spi_default)->dr = 0xFF;
+        spi_get_hw(spi_default)->dr = spi_tx_idle_value;
     }
 }
 
@@ -91,11 +194,23 @@ void setup_spi() {
 
 #define CMD0 0
 #define CMD8 8
+#define CMD13 13
+#define CMD17 17
+#define CMD24 24
 #define CMD55 55
 #define CMD58 58
 
 #define ACMD41 41
 
+#define DATA_START_BLOCK 0XFE
+#define DATA_RES_ACCEPTED 0X05
+
+void spi_clear_read_buf(spi_inst_t *spi) {
+    uint8_t i;
+    while(spi_is_readable(spi)) {
+        i = (uint8_t)spi_get_hw(spi)->dr;
+    }
+}
 
 void spi_read(spi_inst_t *spi, uint8_t* dst, size_t len) {
     for(size_t i = 0; i < len; ++i) {
@@ -105,17 +220,47 @@ void spi_read(spi_inst_t *spi, uint8_t* dst, size_t len) {
     }
 }
 
+void spi_read_large(spi_inst_t *spi, uint8_t* dst, size_t len) {
+    for(size_t i = 0; i < len; ++i) {
+        while(!spi_is_readable(spi))
+            tight_loop_contents();
+        *dst++ = (uint8_t)(spi_get_hw(spi)->dr);
+        // Hack: ensure TX buffer is always filled
+        spi_irq_handler();
+    }
+}
+
 void spi_write(spi_inst_t *spi, uint8_t* src, size_t len) {
     for(size_t i = 0; i < len; ++i) {
-        while(!spi_is_writable(spi)) 
+        while(!spi_is_writable(spi))
             tight_loop_contents();
         spi_get_hw(spi)->dr = (uint32_t)src[i];
     }
 }
 
+void spi_write_const(spi_inst_t *spi, uint8_t val, size_t len) {
+    for(size_t i = 0; i < len; ++i) {
+        while(!spi_is_writable(spi))
+            tight_loop_contents();
+        spi_get_hw(spi)->dr = (uint32_t)val;
+    }
+}
+
+uint32_t get_block_num(uint8_t* arg) {
+    return (
+        (uint32_t)arg[0] << 24 |
+        (uint32_t)arg[1] << 16 |
+        (uint32_t)arg[2] << 8 |
+        (uint32_t)arg[3]
+    );
+}
+
 void handle_cmd(uint8_t* cmd_buf) {
     static bool app_mode = false;
+    uint8_t* arg = cmd_buf + 1;
     uint8_t cmd = cmd_buf[0] & 0b00111111;
+    uint32_t block;
+    Sector* sector;
     uint8_t chksum = cmd_buf[5];
     uint8_t resp[5];
     if(!app_mode) {
@@ -124,8 +269,7 @@ void handle_cmd(uint8_t* cmd_buf) {
                 printf("Handling reset CMD0\n");
                 // TODO: integrity check?
                 // reset always responds with 0x1 (in idle mode?)
-                resp[0] = 0x01;
-                spi_write(spi_default, resp, 1);
+                spi_write_const(spi_default, 0x01, 1);
                 printf("Response sent\n");
                 break;
             case CMD8:
@@ -140,11 +284,82 @@ void handle_cmd(uint8_t* cmd_buf) {
                 spi_write(spi_default, resp, 5);
                 printf("Response sent\n");
                 break;
+            case CMD13:
+                printf("Handling CSD CMD13\n");
+                // Return an R2 response
+                spi_write_const(spi_default, 0x00, 2);
+                printf("Response sent\n");
+                break;
+            case CMD17:
+                printf("Handling read block CMD17\n");
+                // Return an R1 response
+                spi_write_const(spi_default, 0x00, 1);
+                printf("Response sent\n");
+                // Because we are pretending to be an SDHC
+                // the arg is always block number, for SD this value is
+                // an address
+                block = get_block_num(arg);
+                printf("Sending block %d\n", block);
+                sector = getBlock(block);
+                // Disable IRQ, we don't want the buffer to accidentally be filled with idle data
+                // TODO: idk make this parametric
+                irq_set_enabled(SPI0_IRQ, false);
+                // Indicate data is ready to read
+                spi_write_const(spi_default, DATA_START_BLOCK, 1);
+                if (sector == NULL) {
+                    printf("block doesn't exist, returning zeros\n");
+                    // Write data plus CRC
+                    spi_write_const(spi_default, 0x00, SECTOR_SIZE + 2);
+                } else {
+                    // Write data plus CRC
+                    spi_write(spi_default, sector->data, SECTOR_SIZE + 2);
+                }
+                // turn interrupt back on, ensure buffer is full again
+                // TODO: idk make this parametric
+                irq_set_enabled(SPI0_IRQ, true);
+                spi_irq_handler();
+                printf("Finished sending block\n");
+                break;
+            case CMD24:
+                printf("Handling write block CMD24\n");
+                // Because we are pretending to be an SDHC
+                // the arg is always block number, for SD this value is
+                // an address
+                block = get_block_num(arg);
+                printf("Writing to block %d\n", block);
+                // Make sure the block is actually available for writing first
+                sector = allocateBlock(block);
+                // Ensure read buffer is cleared so that we can recieve the start block right away
+                spi_clear_read_buf(spi_default);
+                // Return an R1 response
+                spi_write_const(spi_default, 0x00, 1);
+                // Arduino lib expects the response to be sent on the transfer immediately after
+                // the data/crc, it's too slow to manually send that so instead we start filling
+                // the buffer with the response
+                spi_tx_idle_value = DATA_RES_ACCEPTED;
+                // Wait for data start block, use resp array
+                //printf("Waiting for data start\n");
+                do {
+                    spi_read(spi_default, resp, 1);
+                } while(resp[0] != DATA_START_BLOCK);
+                //printf("Writing data\n");
+                // SPI interrupt doesn't seem to fire here if do a large read
+                // Seems like if we use spi_read 0xFF still gets filled to the bus
+                // until the transfer ends, and then we get a bunch of 0x00 instead,
+                // DATA_RES_ACCEPTED never shows up on the bus
+                spi_read_large(spi_default, sector->data, SECTOR_SIZE + 2);
+                // return idle value back to normal
+                spi_tx_idle_value = 0xFF;
+                //// Respond with accepted
+                //spi_write_const(spi_default, DATA_RES_ACCEPTED, 1);
+                //printf("Data written:\n");
+                //printbuf(sector->data, DATA_START_BLOCK);
+                //printf("CRC written: %x\n", sector->crc);
+                break;
             case CMD55:
                 printf("Handling app CMD55\n");
                 app_mode = true;
-                resp[0] = 0x01;
-                spi_write(spi_default, resp, 1);
+                spi_write_const(spi_default, 0x01, SECTOR_SIZE + 2);
                 printf("Response sent\n");
                 break;
             case CMD58:
